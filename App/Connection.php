406bbb1adb20bf4513ddcde859aa0c67a558e6a3
WilliamAndreoli <willandreoli33@gmail.com>
2024-06-01 23:33:45 -0300

    Registrando Usuarios

--- App/Connection.php
+++ App/Connection.php
@@ -10,7 +10,7 @@ class Connection {
         try {
 
             $conn = new PDO(
-                "mysql:host=localhost;dbname=mvc;charset=utf8",
+                "mysql:host=localhost;dbname=twitter_clone;charset=utf8",
                 "root",
                 ""
             );
