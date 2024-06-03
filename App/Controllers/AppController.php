88ce3e73abdb9217acb61e1045b2c543ffce521d
WilliamAndreoli <willandreoli33@gmail.com>
2024-06-03 16:59:34 -0300

    Criando a Timeline, incluindo e listando Tweets

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -5,16 +5,64 @@ namespace App\Controllers;
 use MF\Controller\Action;
 use MF\Model\Container;
 
-class AppController extends Action {
+class AppController extends Action
+{
 
-    public function timeline() {
+    public function timeline()
+    {
 
         session_start();
 
-        if($_SESSION['id'] != '' && $_SESSION['nome'] != '') {
-            $this->render('timeline');
+        $this->validaAutenticacao();
+
+        //recuperação dos tweets
+        $tweet = Container::getModel('tweet');
+
+        $tweet->__set('id_usuario', $_SESSION['id']);
+
+        $tweets = $tweet->getAll();
+
+        $this->view->tweets = $tweets;
+
+        $this->render('timeline');
+
+
+    }
+
+    public function tweet()
+    {
+
+        session_start();
+
+        $this->validaAutenticacao();
+
+        $tweet = Container::getModel('Tweet');
+
+        if ($_POST['tweet'] != '') {
+
+            $tweet->__set('tweet', $_POST['tweet']);
+            $tweet->__set('id_usuario', $_SESSION['id']);
+
+            $tweet->salvar();
+
+            header('Location: /timeline');
+
         } else {
-            header('Location: /?login=erro');
+
+            header('Location: /timeline?tweet=erro');
+
+        }
+
+
+    }
+
+    public function validaAutenticacao()
+    {
+
+        session_start();
+
+        if (!isset($_SESSION['id']) || $_SESSION['id'] == '' || !isset($_SESSION['nome']) || $_SESSION['nome'] == '') {
+            header('Location /?login=erro');
         }
 
     }
